2cce0f1ab0c704b0e08c61da7c3038038a919891
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-11-16 15:47:28 +0530

    Add the service class.

---
+++ src/main/java/org/example/services/StringService.java
@@ -0,0 +1,27 @@
+package org.example.services;
+
+import org.example.util.StringUtils;
+
+public class StringService {
+    private StringUtils stringUtils;
+
+    public StringService() {
+
+        stringUtils = new StringUtils();
+    }
+
+    public boolean isPalindrome(String word) {
+        if (word == null || word.length() <= 1) {
+            return false;
+        }
+        word = stringUtils.convertToLowerCase(word);
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
